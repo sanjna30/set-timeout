f1841edf41b11d9569211252502b0a5cae8c2423
unknown <sanjnameena74@gmail.com>
2025-01-01 00:47:11 -0500

    add some new things

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [isSending, setIsSending] = useState(false);
+  const [countdown, setCountdown] = useState(5);
+  const [isSent, setIsSent] = useState(false);
+
+  const sendFriendRequest = () => {
+    setIsSending(true);
+    setIsSent(false);
+    setCountdown(5);
+
+    const interval = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      setIsSending(false);
+      setIsSent(true);
+    }, 5000);
+  };
+
   return (
-    <div>kuch change kiya mene abhi 12:40</div>
-  )
-}
+    <div className="container">
+      <h1 className="title">Friend Request</h1>
+      <button className="send-button" onClick={sendFriendRequest} disabled={isSending}>
+        {isSending ? "Sending..." : "Send Friend Request"}
+      </button>
+      {isSending && <p className="countdown">Time left: {countdown}s</p>}
+      {isSent && <p className="success">Friend Request Sent!</p>}
+    </div>
+  );
+};
 
-export default App
+export default App;
